504e6b2d7b2d28473cb32a78b9b49011718b7119
Pamela <pamel@DESKTOP-M81v5LL>
2023-10-02 17:35:57 -0600

    first

---
+++ src/conversorMonedas/ConvertirMonedas.java
@@ -0,0 +1,37 @@
+package conversorMonedas;
+
+import javax.swing.JOptionPane;
+
+public class ConvertirMonedas {
+
+	
+	public void ConvertirPesosADolares(double valor) {
+		double monedaDolar = valor / 17.43;
+		monedaDolar = (double) Math.round(monedaDolar *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaDolar+ " Dolares");
+	}
+	
+	public void ConvertirPesosAEuros(double valor) {
+		double monedaEuro = valor / 18.43;
+		monedaEuro = (double) Math.round(monedaEuro *100d)/100;
+		JOptionPane.showMessageDialog(null, "Tienes $ " +monedaEuro+ " Euros");
+	}
+	
+	public void ConvertirPesosALibras(double valor) {
+		double monedaLibra = valor / 21.26;
+        monedaLibra = (double) Math.round(monedaLibra *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaLibra+ " Libras Esterlinas");
+	}
+	
+	public void ConvertirPesosAYen(double valor) {
+		double monedaYen = valor / 0.12;
+        monedaYen = (double) Math.round(monedaYen *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaYen+ " Yuanes");
+	}
+	
+	public void ConvertirPesosAWon(double valor) {
+		double monedaWon = valor / 0.013;
+        monedaWon = (double) Math.round(monedaWon *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaWon+ " Wons");
+	}
+}
---
+++ src/conversorMonedas/ConvertirMonedasAPesos.java
@@ -0,0 +1,35 @@
+package conversorMonedas;
+
+import javax.swing.JOptionPane;
+
+public class ConvertirMonedasAPesos {
+	public void ConvertirDolaresAPesos(double valor) {
+		double monedaDolar = valor * 17.43;
+		monedaDolar = (double) Math.round(monedaDolar *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaDolar+ " Pesos Mexicanos");
+	}
+	
+	public void ConvertirEurosAPesos(double valor) {
+		double monedaEuro = valor * 18.43;
+		monedaEuro = (double) Math.round(monedaEuro *100d)/100;
+		JOptionPane.showMessageDialog(null, "Tienes $ " +monedaEuro+ " Pesos Mexicanos");
+	}
+	
+	public void ConvertirLibrasAPesos(double valor) {
+		double monedaLibra = valor * 21.26;
+        monedaLibra = (double) Math.round(monedaLibra *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaLibra+ " Pesos Mexicanos");
+	}
+	
+	public void ConvertirYenAPesos(double valor) {
+		double monedaYen = valor *0.12;
+        monedaYen = (double) Math.round(monedaYen *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaYen+ " Pesos Mexicanos");
+	}
+	
+	public void ConvertirWonAPesos(double valor) {
+		double monedaWon = valor * 0.013;
+        monedaWon = (double) Math.round(monedaWon *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaWon+ " Pesos Mexicanos");
+	}
+}
